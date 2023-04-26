d9a2a8a7063dac0757bec93887cd2af6ec84eceb
LatyshevArtem <artem.l.2002@mail.ru>
2023-04-26 17:30:54 +1000

    Added a function that returns a string for each query parameter
    And also added a function that collects all query strings into one .And from the response with paintings take the total number of pictures for this query

--- src/api/painting-service.js
+++ src/api/painting-service.js
@@ -2,30 +2,63 @@ import { BASE_URL } from '../consts';
 
 const paintingsUrl = `${BASE_URL}/paintings`;
 
+const getQueryByPage = (page) => `_page=${page}`;
+
+const getQueryByLimit = (limit) => `_limit=${limit}`;
+
+const getQueryByName = (name) => {
+  const formattedName = name.trim();
+  return formattedName && `name_like=${new RegExp(`^${formattedName}`, 'ig')}`;
+};
+
+const getQueryByAuthorId = (authorId) => authorId && `authorId=${authorId}`;
+
+const getQueryByLocationId = (locationId) => locationId && `locationId=${locationId}`;
+
+const getQueryByDateFrom = (dateFrom) => {
+  const formattedDateFrom = dateFrom.trim();
+  return formattedDateFrom && `created_gte=${formattedDateFrom.padStart(4, '0')}`;
+};
+
+const getQueryByDateBefore = (dateBefore) => {
+  const formattedDateBefore = dateBefore.trim();
+  return (
+    formattedDateBefore &&
+    (formattedDateBefore.length > 4
+      ? `created.length_lte=${formattedDateBefore.length}`
+      : `created_lte=${formattedDateBefore.padStart(4, '0')}`)
+  );
+};
+
+const getFullQuery = (...queries) => queries.filter((query) => query).join('&');
+
 export default class PaintingService {
   static async getPaintings(page, limit, name, authorId, locationId, dateFrom, dateBefore) {
-    const queryByPage = `_page=${page}`;
-    const queryByLimit = `_limit=${limit}`;
-    const queryByName = name && `name_like=${name.trim()}`;
-    const queryByAuthorId = authorId && `authorId=${authorId}`;
-    const queryByLocationId = locationId && `locationId=${locationId}`;
-    const queryByDateFrom = dateFrom && `created_gte=/d/${dateFrom.trim()}`;
-    const queryByDateBefore = dateBefore && `created_lte=${dateBefore.trim()}`;
+    const queryByPage = getQueryByPage(page);
+    const queryByLimit = getQueryByLimit(limit);
+    const queryByName = getQueryByName(name);
+    const queryByAuthorId = getQueryByAuthorId(authorId);
+    const queryByLocationId = getQueryByLocationId(locationId);
+    const queryByDateFrom = getQueryByDateFrom(dateFrom);
+    const queryByDateBefore = getQueryByDateBefore(dateBefore);
 
-    const queryString = [
+    const queryString = getFullQuery(
       queryByPage,
       queryByLimit,
       queryByName,
       queryByAuthorId,
       queryByLocationId,
       queryByDateFrom,
-      queryByDateBefore,
-    ]
-      .filter((value) => value)
-      .join('&');
+      queryByDateBefore
+    );
 
     const response = await fetch(`${paintingsUrl}?${queryString}`);
     const paintings = await response.json();
-    return paintings;
+    const paintingsTotalCount = response.headers.get('X-Total-Count');
+
+    return {
+      paintings,
+      paintingsTotalCount,
+    };
   }
 }
