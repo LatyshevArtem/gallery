4edd75554717d9985183be942d16e7c38884715a
LatyshevArtem <artem.l.2002@mail.ru>
2023-04-27 15:11:59 +1000

    Changed the logic of the pagination block
    Now, if the number of pages is equal to one, then the pagination block is displayed completely, but all buttons are disabled. If there are two pages, then two buttons with numbers one and two are displayed. If there are three or more pages, then the pagination block works by default. Also moved the logic of the work of individual elements into separate functions. And edited the styles to correct display if the number of pages  is equal to two

--- src/components/ui/pagination/Pagination.jsx
+++ src/components/ui/pagination/Pagination.jsx
@@ -6,50 +6,86 @@ import styles from './styles.module.scss';
 const cx = cn.bind(styles);
 
 const Pagination = ({ сlassName, page, endPageNumber, onChangePage }) => {
-  const numbersOfPage = [
-    getPageNumber(1, page, endPageNumber),
-    getPageNumber(2, page, endPageNumber),
-    getPageNumber(3, page, endPageNumber),
-  ];
+  const handleArrowNavigateButtonClick = (isDoubleArrow, isForwardArrow) => {
+    if (isDoubleArrow && !isForwardArrow && isLeftArrowNavigateButtonAvailable) {
+      onChangePage(1);
+    } else if (!isDoubleArrow && !isForwardArrow && isLeftArrowNavigateButtonAvailable) {
+      onChangePage((prev) => prev - 1);
+    } else if (!isDoubleArrow && isForwardArrow && isRightArrowNavigateButtonAvailable) {
+      onChangePage((prev) => prev + 1);
+    } else if (isDoubleArrow && isForwardArrow && isRightArrowNavigateButtonAvailable) {
+      onChangePage(endPageNumber);
+    }
+  };
+
+  const checkIsNumberNavigateButtonNotAvailable = (pageNumber) => pageNumber > endPageNumber;
+
+  const checkIsNumberNavigateButtonActive = (pageNumber) => pageNumber === page;
+
+  const handleNumberNavigateButtonClick = (pageNumber) => {
+    return pageNumber <= endPageNumber ? onChangePage(pageNumber) : () => {};
+  };
+
+  const getNumbersOfPage = () => {
+    const numbersOfPage = [
+      getPageNumber(1, page, endPageNumber),
+      getPageNumber(2, page, endPageNumber),
+    ];
+
+    if (endPageNumber > 2 || endPageNumber === 1) {
+      numbersOfPage.push(getPageNumber(3, page, endPageNumber));
+    }
+
+    return numbersOfPage;
+  };
+
+  const numbersOfPage = getNumbersOfPage();
+
+  const isLeftArrowNavigateButtonAvailable = page !== 1;
+  const isLeftArrowNavigateButtonNotAvailable = !isLeftArrowNavigateButtonAvailable;
+
+  const isRightArrowNavigateButtonAvailable = page !== endPageNumber;
+  const isRightArrowNavigateButtonNotAvailable = !isRightArrowNavigateButtonAvailable;
 
   return (
     <ul className={cx(сlassName, 'pagination')}>
       <li
         className={cx('pagination-item', {
-          'pagination-item--disabled': page === 1,
+          'pagination-item--disabled': isLeftArrowNavigateButtonNotAvailable,
         })}
-        onClick={page !== 1 ? () => onChangePage(1) : () => {}}>
+        onClick={() => handleArrowNavigateButtonClick(true, false)}>
         <NavigateArrow doubleArrow={true} back={true} />
       </li>
       <li
         className={cx('pagination-item', {
-          'pagination-item--disabled': page === 1,
+          'pagination-item--disabled': isLeftArrowNavigateButtonNotAvailable,
         })}
-        onClick={page !== 1 ? () => onChangePage((prev) => prev - 1) : () => {}}>
+        onClick={() => handleArrowNavigateButtonClick(false, false)}>
         <NavigateArrow back={true} />
       </li>
       {numbersOfPage.map((pageNumber) => (
         <li
           className={cx('pagination-item', 'pagination-item--number', {
-            'pagination-item--active': pageNumber === page,
+            'pagination-item--disabled': checkIsNumberNavigateButtonNotAvailable(pageNumber),
+            'pagination-item--active': checkIsNumberNavigateButtonActive(pageNumber),
           })}
           key={pageNumber}
-          onClick={() => onChangePage(pageNumber)}>
+          onClick={() => handleNumberNavigateButtonClick(pageNumber)}>
           {pageNumber}
         </li>
       ))}
       <li
         className={cx('pagination-item', {
-          'pagination-item--disabled': page === endPageNumber,
+          'pagination-item--disabled': isRightArrowNavigateButtonNotAvailable,
         })}
-        onClick={page !== endPageNumber ? () => onChangePage((prev) => prev + 1) : () => {}}>
+        onClick={() => handleArrowNavigateButtonClick(false, true)}>
         <NavigateArrow />
       </li>
       <li
         className={cx('pagination-item', {
-          'pagination-item--disabled': page === endPageNumber,
+          'pagination-item--disabled': isRightArrowNavigateButtonNotAvailable,
         })}
-        onClick={page !== endPageNumber ? () => onChangePage(endPageNumber) : () => {}}>
+        onClick={() => handleArrowNavigateButtonClick(true, true)}>
         <NavigateArrow doubleArrow={true} />
       </li>
     </ul>
