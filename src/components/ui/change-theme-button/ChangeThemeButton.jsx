f60c61f2d00be8961b77d5b424a11b51e71e0f4c
LatyshevArtem <artem.l.2002@mail.ru>
2023-05-04 20:36:50 +1000

    Theme change button now changes the application theme on click
    Added connection of styles through the classnames library. Code refactoring in scss file

--- src/components/ui/change-theme-button/ChangeThemeButton.jsx
+++ src/components/ui/change-theme-button/ChangeThemeButton.jsx
@@ -1,10 +1,17 @@
+import cn from 'classnames/bind';
+import { useContext } from 'react';
+import ThemeContex from '../../../contexts/ThemeContext';
 import { ReactComponent as ChangeThemeButtonIcon } from '../../../assets/images/change-theme-button-icon.svg';
 import styles from './styles.module.scss';
 
-const ChangeThemeButton = () => {
+const cx = cn.bind(styles);
+
+const ChangeThemeButton = ({ className }) => {
+  const { toggleTheme } = useContext(ThemeContex);
+
   return (
-    <button className={styles['change-theme-button']} type="button">
-      <ChangeThemeButtonIcon />
+    <button className={cx(className, 'change-theme-button')} onClick={toggleTheme} type="button">
+      <ChangeThemeButtonIcon className={cx('change-theme-button__icon')} />
     </button>
   );
 };
