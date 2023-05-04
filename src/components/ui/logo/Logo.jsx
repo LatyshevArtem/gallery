2dd2d0949ae3582996d82cf60a273867dcb46a36
LatyshevArtem <artem.l.2002@mail.ru>
2023-05-04 21:44:34 +1000

    Added connection of styles through the classnames library to the Logo component

--- src/components/ui/logo/Logo.jsx
+++ src/components/ui/logo/Logo.jsx
@@ -1,9 +1,12 @@
+import cn from 'classnames/bind';
 import { ReactComponent as LogoImage } from '../../../assets/images/logo.svg';
 import styles from './styles.module.scss';
 
+const cx = cn.bind(styles);
+
 const Logo = ({ href }) => {
   return (
-    <a className={styles['logo']} href={href}>
+    <a className={cx('logo')} href={href}>
       <LogoImage />
     </a>
   );
