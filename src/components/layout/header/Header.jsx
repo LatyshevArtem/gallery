de4285bcf352a6c3ab8d0ae81b83c540d0d20162
LatyshevArtem <artem.l.2002@mail.ru>
2023-05-04 20:51:32 +1000

    Added information about the topic to the header of the site
    In styles its change the color of the theme switch button depending on the theme

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,4 +1,6 @@
 import cn from 'classnames/bind';
+import { useContext } from 'react';
+import ThemeContex from '../../../contexts/ThemeContext';
 import Logo from '../../ui/logo/Logo';
 import ChangeThemeButton from '../../ui/change-theme-button/ChangeThemeButton';
 import styles from './style.module.scss';
@@ -6,10 +8,16 @@ import styles from './style.module.scss';
 const cx = cn.bind(styles);
 
 const Header = () => {
+  const { isDarkTheme } = useContext(ThemeContex);
+
   return (
     <header className={cx('header')}>
       <Logo />
-      <ChangeThemeButton />
+      <ChangeThemeButton
+        className={cx('change-theme-button', {
+          'change-theme-button--dark': isDarkTheme,
+        })}
+      />
     </header>
   );
 };
