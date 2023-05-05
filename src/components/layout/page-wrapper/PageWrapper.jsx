003ae196dd5bcb89a662c7954f0e0e727be251ce
LatyshevArtem <artem.l.2002@mail.ru>
2023-05-05 13:15:47 +1000

    Added information about the current application theme to the component. Code refactoring

--- src/components/layout/page-wrapper/PageWrapper.jsx
+++ src/components/layout/page-wrapper/PageWrapper.jsx
@@ -1,10 +1,21 @@
 import cn from 'classnames/bind';
+import { useContext } from 'react';
+import ThemeContext from '../../../contexts/ThemeContext';
 import styles from './styles.module.scss';
 
 const cx = cn.bind(styles);
 
 const PageWrapper = ({ children }) => {
-  return <div className={cx('page-wrapper')}>{children}</div>;
+  const { isDarkTheme } = useContext(ThemeContext);
+
+  return (
+    <div
+      className={cx('page-wrapper', {
+        'page-wrapper--dark': isDarkTheme,
+      })}>
+      {children}
+    </div>
+  );
 };
 
 export default PageWrapper;
