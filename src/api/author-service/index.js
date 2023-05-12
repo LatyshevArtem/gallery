51f6d7a99b5196994c05f3e9d67e77fe82f59432
LatyshevArtem <artem.l.2002@mail.ru>
2023-05-12 13:52:28 +1000

    Also created a separate folder for services and renamed the files with services to index.js, similar to painting-service. Also moved the url path for fetch into a separate constant

--- src/api/author-service/index.js
+++ src/api/author-service/index.js
@@ -1,8 +1,10 @@
-import { BASE_URL } from '../consts';
+import { BASE_URL } from '../../consts';
+
+const authorsUrl = `${BASE_URL}/authors`;
 
 export default class AuthorService {
   static async getAuthors() {
-    const response = await fetch(`${BASE_URL}/authors`);
+    const response = await fetch(authorsUrl);
     const authors = await response.json();
     return authors;
   }
--- src/api/location-service/index.js
+++ src/api/location-service/index.js
@@ -1,8 +1,10 @@
-import { BASE_URL } from '../consts';
+import { BASE_URL } from '../../consts';
+
+const locationsUrl = `${BASE_URL}/locations`;
 
 export default class LocationService {
   static async getLocations() {
-    const response = await fetch(`${BASE_URL}/locations`);
+    const response = await fetch(locationsUrl);
     const locations = await response.json();
     return locations;
   }
